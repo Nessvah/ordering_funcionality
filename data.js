2ab8044928577dcd6d4b34ea950595a294e9cc73
Nessvah <info@silviavanessa.dev>
2023-02-23 11:09:45 +0000

    style menu container and change data emoji to pic

--- data.js
+++ data.js
@@ -4,20 +4,20 @@ const menuArray = [
         ingredients: ["pepperoni", "mushrom", "mozarella"],
         id: 0,
         price: 14,
-        emoji: "🍕"
+        img: `/Images/pizza.png`
     },
     {
         name: "Hamburger",
         ingredients: ["beef", "cheese", "lettuce"],
         price: 12,
-        emoji: "🍔",
+        img: `/Images/burguer.png`,
         id: 1
     },
         {
         name: "Beer",
         ingredients: ["grain, hops, yeast, water"],
         price: 12,
-        emoji: "🍺",
+        img: '/Images/beer.png',
         id: 2
     }
 ]
